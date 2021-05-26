685730e03e98c51348ec9441f10c7bab8d238d7d
Catzzz <chuneleu@gmail.com>
2021-05-26 10:45:00 -0700

    fetch data from json and show

--- src/App.js
+++ src/App.js
@@ -2,37 +2,48 @@ import React, { Component } from "react";
 import { robots } from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import './App.css';
+import "./App.css";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
     this.onSearchChange = this.onSearchChange.bind(this);
   }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
 
-  onSearchChange(event){
-    this.setState ({
-        searchfield: event.target.value
-    })
+  onSearchChange(event) {
+    this.setState({
+      searchfield: event.target.value,
+    });
     console.log(event.target.value);
-
-
-
   }
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-        return robots.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
-    })
-    return (
-      <div className="tc">
-        <h1 className='f1'> RobotFriends</h1>
-        <SearchBox searchChange = {this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
+    if(this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    }else{
+      return (
+        <div className="tc">
+          <h1 className="f1"> RobotFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
+    
   }
 }
